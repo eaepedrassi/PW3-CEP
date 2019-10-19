b80b796a7467b05e31df5d518fa44e9bef8d00af
Anderson L. Pedrassi <anderson_pedrassi@hotmail.com>
2019-10-18 22:28:12 -0300

    Exercicio finalizado

--- src/app/service/cep-api.service.ts
+++ src/app/service/cep-api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CepAPIService {
 
-  constructor() { }
+apiURL: string = "https://viacep.com.br/ws/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  handleError(error){
+
+    let errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    alert(errorMessage);
+    return throwError(errorMessage);
+
+  }
+
 }
